f5435cd2a437f00af94868ab277646ef4721ce21
pardhivas <pardhivas@gmail.com>
2019-07-01 06:04:12 +0000

    initial commit

---
+++ git_practice/Hello.java
@@ -0,0 +1,19 @@
+class Hello
+{
+
+  public static void main(String[] args)
+{
+   int i = 10;
+int j  = 20;
+int sum = i + j;
+System.out.println("Sum is: "+sum);
+for(int x = 0; x < 5; x++)
+{
+
+    System.out.println("Hello World " +x);
+}
+   
+}
+
+
+}
